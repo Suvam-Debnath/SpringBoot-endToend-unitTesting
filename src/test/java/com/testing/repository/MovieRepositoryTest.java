369317c913c5adfe0af068adaf00ca9587f3e02a
Suvam Debnath <debnathsuvam322@gmail.com>
2025-10-12 20:34:36 +0530

    tested repository layer

---
+++ src/test/java/com/testing/repository/MovieRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.testing.repository;
+
+import com.testing.model.Movie;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class MovieRepositoryTest {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Test
+    void save(){
+        //Arrange - Setting up the data that required for the test case
+        Movie avatarMovie = new Movie();
+        avatarMovie.setName("Avatar");;
+        avatarMovie.setGenera("Action");
+        avatarMovie.setReleaseDate(LocalDate.of(2000, Month.APRIL,22));
+
+        //Act - Calling a method/Unit that is being tested
+        Movie newMovie = movieRepository.save(avatarMovie);
+
+        //Assert - Verify that the expected result is correct or not
+        assertNotNull(newMovie);
+        assertThat(newMovie.getId()).isNotEqualTo(null);
+
+    }
+
+}
